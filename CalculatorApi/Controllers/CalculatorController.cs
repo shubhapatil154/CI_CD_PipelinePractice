3f586a9187a9ab67b32b19671f3f15eb9ab78b8f
Shubha Suresh Patil <shusures@publicisgroupe.net>
2021-10-24 18:59:57 +0530

    Initial commit

---
+++ CalculatorApi/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {      
+        [HttpGet]
+        public string GetResult()
+        {
+            return "Sum of .....";
+        }
+        
+    }
+}
